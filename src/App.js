55764a3e28311b0764d64ad36fbe2a85ec9802b4
Werwllleh <allvxm4@gmail.com>
2023-11-06 20:47:31 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+import Advanced from './examples/Advanced'
+
+
+function App() {
+
+    return (
+        <div className='app'>
+            <Advanced/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/utils/cards.js
@@ -0,0 +1,22 @@
+export const db = [
+    {
+        name: '5',
+        url: 'https://i.pinimg.com/736x/b6/8f/35/b68f3566cd01a20de38648c5d8eb8020--nature-animals-wild-animals.jpg'
+    },
+    {
+        name: '4',
+        url: 'https://i.pinimg.com/736x/9c/b6/01/9cb601b60ba8b3f8e29ca23a246bee9c--fat-brown-bears.jpg'
+    },
+    {
+        name: '3',
+        url: 'https://img.goodfon.ru/original/640x960/6/82/medved-buryy-sherst-meh.jpg'
+    },
+    {
+        name: '2',
+        url: 'https://i.pinimg.com/736x/94/73/3e/94733efab604f4e17a4c09fe88594418.jpg'
+    },
+    {
+        name: '1',
+        url: 'https://i.pinimg.com/originals/a3/29/38/a329382571051b54114d7fd2a887cc94.jpg'
+    }
+]
